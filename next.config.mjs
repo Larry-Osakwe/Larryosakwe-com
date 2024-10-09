ced8e4ddf552204f5241cf21765d1a251d8e3586
Larry Osakwe <larryosak@gmail.com>
2024-10-09 14:51:01 -0700

    get twin macro working

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   compiler: {
     styledComponents: true,
-  },
-  experimental: {
-    forceSwcTransforms: true,
   },
 }
 
