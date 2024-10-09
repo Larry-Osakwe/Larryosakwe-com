009b4d9396918a5be5df2693d5df93cbf29b3673
Larry Osakwe <larryosak@gmail.com>
2024-10-08 17:30:46 -0700

    initial commit

---
+++ src/lib/openpaneldev/openPanelDevClient.ts
@@ -0,0 +1,8 @@
+import { OpenPanel } from '@openpanel/sdk';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const op = new OpenPanel({
+    clientId: '2892d91e-9c71-4c0b-957e-83b3bd40e841',
+    // mostly for backend and apps that can't rely on CORS
+    clientSecret: '[CLIENT_SECRET]',
+  });
