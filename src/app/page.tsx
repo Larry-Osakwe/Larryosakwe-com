5785d2e2b108e763f3552cb7f4b1f34917c986a9
Larry Osakwe <larryosak@gmail.com>
2024-10-15 16:36:04 -0700

    how it works component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import tw from 'twin.macro';
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero/Hero";
 import { Sponsors } from "@/components/Sponsors";
-import { About } from "@/components/About";
+import { About } from "@/components/About/About";
+import { HowItWorks } from "@/components/HowItWorks";
 
 const Container = tw.div`flex flex-col items-center justify-center min-h-screen py-2`;
 
@@ -15,6 +16,7 @@ export default function Home() {
     <Hero />
     <Sponsors />
     <About />
+    <HowItWorks />
     <Container>
       hi
     </Container>
