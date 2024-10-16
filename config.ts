54b36acbdd4b7afe130190d2d0d969e47edc4b00
Larry Osakwe <larryosak@gmail.com>
2024-10-16 16:57:41 -0700

    update config

--- config.ts
+++ config.ts
@@ -1,11 +1,13 @@
 export const config = {
-  // TODO: Replace with your app name
+  // REQUIRED   : Replace with your app name
   appName: "FlareStack",
+  // REQUIRED: A short description of your app for SEO tags 
   appDescription: "Your app description here",
+  // REQUIRED: Your domain name (No http:// or https://, no trailing slash, no www, just the naked domain)
   domainName: "flarestack.io",
+  stripe: {
 
-  // External service configurations
-  githubUrl: "https://github.com/yourusername/your-repo",
+  },
 
   // Authentication paths
   auth: {
