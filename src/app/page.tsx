7c5c16adf1e1f1596c84a4f85bef6a43502ccebb
Larry Osakwe <larryosak@gmail.com>
2024-11-18 18:06:19 -0800

    clean up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,15 @@
-'use client'
-import * as React from "react"
-import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero/Hero";
-import { Sponsors } from "@/components/Sponsors";
-import { About } from "@/components/About/About";
-import { HowItWorks } from "@/components/HowItWorks";
-import { Features } from "@/components/Features";
-import { Services } from "@/components/Services";
-import { CTA } from "@/components/CTA";
-import { Testimonials } from "@/components/Testimonials";
-import { Team } from "@/components/Team";
-import { Pricing } from "@/components/Pricing";
-import { Newsletter } from "@/components/Newsletter";
-import { FAQ } from "@/components/FAQ";
-import { Footer } from "@/components/Footer";
-import { ScrollToTop } from "@/components/ScrollToTop";
+import { HomeContent } from "@/components/HomeContent/HomeContent";
+import { getSEOTags } from "@/lib/seo/seo";
+
+export const metadata = getSEOTags({
+  title: "Home",
+  canonicalUrlRelative: "/",
+});
 
 export default function Home() {
   return (
     <>
-    <Navbar />
-    <Hero />
-    <Pricing />
-    <Sponsors />
-    <About />
-    <HowItWorks />
-    <Features />
-    <Pricing />
-    <Testimonials />
-    <Services />
-    <FAQ />
-    <CTA />
-    
-    <Team />
-    
-    <Newsletter />
-    
-    <Footer />
-    hi. hello. goodbye
-    <ScrollToTop />
+      <HomeContent />
     </>
   );
 }
